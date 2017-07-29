10ecbad3cc51468fb4661c5b2934de9120515aea
Dmitrii K <winston.smith.spb@gmail.com>
2017-07-29 21:27:15 +0700

    refactoring

--- code.py
+++ code.py
@@ -1,35 +1,45 @@
 import pywapi
 
-city = int(input("Choose your city: \n 1 - Moscow, 2 - Minsk, 3 - Saint-Petersburg, 4 - New York, 5 - London, 6 - Bristol, 7 - Kiev, 8 - Los Angeles, 9 - Jerusalim, 10 - Warsaw.   "))
-if city == 1:
-    weather_com_result = pywapi.get_weather_from_weather_com('RSXX0063')
-    print("Weather.com says: It is " + str(weather_com_result['current_conditions']['text']) + " and " + weather_com_result['current_conditions']['temperature'] + "C now in Moscow.\n\n")
-elif city == 2:
-    weather_com_result=pywapi.get_weather_from_weather_com('BOXX0014')
-    print("Weather.com says: It is " + str(weather_com_result['current_conditions']['text']) + " and " + weather_com_result['current_conditions']['temperature'] + "C now in Minsk.\n\n")
-elif city == 3:
-    weather_com_result=pywapi.get_weather_from_weather_com('RSXX0091')
-    print("Weather.com says: It is " + str(weather_com_result['current_conditions']['text']) + " and " + weather_com_result['current_conditions']['temperature'] + "C now in Saint-Petersburg.\n\n")
-elif city == 4:
-    weather_com_result = pywapi.get_weather_from_weather_com('USNY0996')
-    print("Weather.com says: It is " + str(weather_com_result['current_conditions']['text']) + " and " + weather_com_result['current_conditions']['temperature'] + "C now in New York.\n\n")
-elif city == 5:
-    weather_com_result = pywapi.get_weather_from_weather_com('UKXX0085')
-    print("Weather.com says: It is " + str(weather_com_result['current_conditions']['text']) + " and " + weather_com_result['current_conditions']['temperature'] + "C now in London.\n\n")
-elif city == 6:
-    weather_com_result=pywapi.get_weather_from_weather_com('UKXX0025')
-    print("Weather.com says: It is " + str(weather_com_result['current_conditions']['text']) + " and " + weather_com_result['current_conditions']['temperature'] + "C now in Bristol.\n\n")
-elif city == 7:
-    weather_com_result=pywapi.get_weather_from_weather_com('UPXX0486')
-    print("Weather.com says: It is " + str(weather_com_result['current_conditions']['text']) + " and " + weather_com_result['current_conditions']['temperature'] + "C now in Kiev.\n\n")
-elif city == 8:
-    weather_com_result=pywapi.get_weather_from_weather_com('USCA0638')
-    print("Weather.com says: It is " + str(weather_com_result['current_conditions']['text']) + " and " + weather_com_result['current_conditions']['temperature'] + "C now in Los Angeles.\n\n")
-elif city == 9:
-    weather_com_result=pywapi.get_weather_from_weather_com('ISXX0010')
-    print("Weather.com says: It is " + str(weather_com_result['current_conditions']['text']) + " and " + weather_com_result['current_conditions']['temperature'] + "C now in Jerusalim.\n\n")
-elif city == 10:
-    weather_com_result=pywapi.get_weather_from_weather_com('PLXX0028')
-    print("Weather.com says: It is " + str(weather_com_result['current_conditions']['text']) + " and " + weather_com_result['current_conditions']['temperature'] + "C now in Warsaw.\n\n")
+CITIES = {
+    1: {'name': 'Moscow', 'code': 'RSXX0063'},
+    2: {'name': 'Minsk', 'code': 'BOXX0014'},
+    3: {'name': 'Saint-Petersburg', 'code': 'RSXX0091'},
+    4: {'name': 'New York', 'code': 'USNY0996'},
+    5: {'name': 'London', 'code': 'UKXX0085'},
+    6: {'name': 'Bristol', 'code': 'UKXX0025'},
+    7: {'name': 'Kiev', 'code': 'UPXX0486'},
+    8: {'name': 'Los Angeles', 'code': 'USCA0638'},
+    9: {'name': 'Jerusalim', 'code': 'ISXX0010'},
+    10: {'name': 'Warsaw', 'code': 'PLXX0028'},
+}
 
 
+def get_choose():
+    for city_key, city_val in CITIES.items():
+        print('{} - {}'.format(city_key, city_val['name']))
+    city = int(input('\nChoose your city: '))
+    if city in CITIES:
+        return CITIES[city]
+    else:
+        return None
+
+
+def get_weather(city):
+    return pywapi.get_weather_from_weather_com(city['code'])
+
+
+def print_weather(weather, city):
+    weather_current = weather['current_conditions']
+    print('Weather.com says: It is {} and {} C now in {}.\n\n'
+          .format(weather_current['text'],
+                  weather_current['temperature'],
+                  city['name']))
+
+
+if __name__ == '__main__':
+    city = get_choose()
+    if city:
+        weather = get_weather(city)
+        print_weather(weather, city)
+    else:
+        print('Wrong city code')
